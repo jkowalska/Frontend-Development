e031a64fbfd4d6c6296d116424aa7be7fc2b4fe8
jkowalska <asia528@wp.pl>
2017-03-23 20:51:40 +0100

    Zadanie 2

---
+++ Zadanie2/Film.js
@@ -0,0 +1,59 @@
+class Film {
+	constructor(json) {
+		this.tytul = json.tytul;
+		this.rezyser = json.rezyser;
+		this.rok = json.rok;
+		this.gatunek = json.gatunek;
+		this.kraj = json.kraj;
+	}
+
+	toTableRow() {
+		return `<tr><td>
+		${this.tytul}</td><td>
+		${this.rezyser}</td><td>
+		${this.rok}</td><td>
+		${this.gatunek}</td><td>
+		${this.kraj}</tr></td>`;
+	}
+}
+
+class listOfFilmy {
+	constructor(){
+		this.filmy = [];
+		let self = this;
+	}
+	
+	addFilm(json) {
+		this.filmy.push(new Film(json));
+	}
+
+	toTable() {
+		let table = `<tr><th>Tytul</th> <th>Rezyser</th> <th>Rok</th> <th>Gatunek</th> <th>Kraj</th></tr>`;
+		
+		for(let i=0; i<this.filmy.length; i++) {
+			table += this.filmy[i].toTableRow();
+		}
+
+		table += `</table>`;
+		return table;
+	}
+}
+
+function init() {
+	let oReq = new XMLHttpRequest();
+	oReq.onload = reqListener;
+	oReq.open("get", "./data.json", true);
+	oReq.send();
+	let json;
+
+	function reqListener(e) {
+   		json = JSON.parse(this.responseText);
+   		listOfFilmy = new listOfFilmy();
+    	for(let i=0; i<json.length; i++) {
+        	listOfFilmy.addFilm(json[i]);
+        }
+
+    	let context = document.getElementById('table');
+		context.innerHTML = listOfFilmy.toTable();
+	}
+}
